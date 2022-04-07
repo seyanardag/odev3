4d9c779ba5e6c44dcd274f3ef5b65e3326c0ab99
Seyf <seyanardag@gmail.com>
2022-04-07 17:52:01 +0300

    bug

--- bank.js
+++ bank.js
@@ -1,43 +1,55 @@
 // k => bir seferlik para çekme limiti (number),
 // amounts => müşterilerin ilk sıralamasına göre oluşturulan hesap bakiyeleri (array)
 
-
-
 let k = 2;
-let amounts = [3, 5, 1, 2, 7];
+let amounts = [3, 2, 1, 2, 7];
 // sıralama = [0, 1, 2, 3, 4];
 
 function getFinalOrder(k, amounts) {
   let lastOrder = [];
   let amountsObject = [];
   let tempObj = amountsObject;
-  
+
   amounts.forEach((element, index) => {
     amountsObject.push({
       no: index,
       amount: element,
     });
   });
-  
-  console.log(amountsObject)
-  let index=0
-  while (amountsObject.length != 0) {
-    
-    console.log("amount:", amountObj);
-    console.log("index:", index);
-    console.log("***************************");
-    if (amountsObject[0] <= k) {
-      //amount k ya eşit veya küçükse
-      amountObj.amount = 0;
-      lastOrder.push(amountObj.no);
-      tempObj.splice(index,1)
-      amountObj=tempObj
-    }
-  }
 
-  amountsObject.map((amountObj, index) => {
-    
-  });
+  console.log(amountsObject);
+  let index = 0;
+  while (amountsObject.length>0) {
+    amountsObject.splice(0,1)
+    lastOrder.push(amountsObject[0].no);
+    console.log(amountsObject);    
+ }
+
+  console.log(amountsObject);
+
+  // while (amountsObject.length == 4) { 
+  //    console.log(amountsObject.length)
+  //   if (amountsObject[index] <= k) {
+  //     //amount k ya eşit veya küçükse
+  //     amountObj.amount = 0;
+  //     lastOrder.push(amountObj.no);
+
+  //     tempObj.splice(index, 1);
+  //     amountObj = tempObj;
+  //     index--
+  //   }
+
+  //   index++;
+  //   if (index == amountsObject.length) {
+  //     index = 0;
+  //   } else if (amountsObject.length == 0) {
+  //     break
+  //   }
+  // }
+
+  // amountsObject.map((amountObj, index) => {
+
+  // });
 
   console.log("SON SATIRDA AMOUNTS OBJECT:", amountsObject);
   console.log("SON SATIRDA LAST ORDER:", lastOrder);
@@ -46,14 +58,9 @@ function getFinalOrder(k, amounts) {
 
 getFinalOrder(k, amounts);
 
-
-
-
-
-
-
-
-
+// console.log("amount:", amountsObject);
+// console.log("index:", index);
+// console.log("***************************");
 
 // amountsObject.map((amountObj, index) => {
 //   console.log("amount:", amountObj);
@@ -66,6 +73,5 @@ getFinalOrder(k, amounts);
 //     tempObj.splice(index,1)
 //     amountObj=tempObj
 //   }
-
 
 // });
