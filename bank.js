7a99cca7c0abd6c383ebd29a0cd10bbc634b1c23
Seyf <seyanardag@gmail.com>
2022-04-08 17:40:08 +0300

    completed

--- bank.js
+++ bank.js
@@ -2,7 +2,7 @@
 // amounts => müşterilerin ilk sıralamasına göre oluşturulan hesap bakiyeleri (array)
 
 let k = 2;
-let amounts = [3, 2, 1, 2, 7];
+let amounts = [3, 9, 0,3, 2, 7];
 // sıralama = [0, 1, 2, 3, 4];
 
 function getFinalOrder(k, amounts) {
@@ -17,61 +17,25 @@ function getFinalOrder(k, amounts) {
     });
   });
 
-  console.log(amountsObject);
+  // console.log(amountsObject);
   let index = 0;
-  while (amountsObject.length>0) {
-    amountsObject.splice(0,1)
-    lastOrder.push(amountsObject[0].no);
-    console.log(amountsObject);    
- }
+  while (amountsObject.length >= 1) {
+    // console.log(amountsObject[index].amount);
+    if (amountsObject[index].amount <= k) {
+      lastOrder.push(amountsObject[index].no);
+      amountsObject.splice(index, 1);
+    } else {
+      amountsObject[index].amount -= k;
+      amountsObject.push(amountsObject[index]);
+      amountsObject.splice(index, 1);
+    }
+  }
 
-  console.log(amountsObject);
+  // console.log("SON SATIRDA AMOUNTS OBJECT:", amountsObject);
+  // console.log("SON SATIRDA LAST ORDER:", lastOrder);
+  // console.log("SON SATIRDA tempObj:", tempObj);
 
-  // while (amountsObject.length == 4) { 
-  //    console.log(amountsObject.length)
-  //   if (amountsObject[index] <= k) {
-  //     //amount k ya eşit veya küçükse
-  //     amountObj.amount = 0;
-  //     lastOrder.push(amountObj.no);
-
-  //     tempObj.splice(index, 1);
-  //     amountObj = tempObj;
-  //     index--
-  //   }
-
-  //   index++;
-  //   if (index == amountsObject.length) {
-  //     index = 0;
-  //   } else if (amountsObject.length == 0) {
-  //     break
-  //   }
-  // }
-
-  // amountsObject.map((amountObj, index) => {
-
-  // });
-
-  console.log("SON SATIRDA AMOUNTS OBJECT:", amountsObject);
-  console.log("SON SATIRDA LAST ORDER:", lastOrder);
-  console.log("SON SATIRDA LAST tempObj:", tempObj);
+  return lastOrder
 }
 
-getFinalOrder(k, amounts);
-
-// console.log("amount:", amountsObject);
-// console.log("index:", index);
-// console.log("***************************");
-
-// amountsObject.map((amountObj, index) => {
-//   console.log("amount:", amountObj);
-//   console.log("index:", index);
-//   console.log("***************************");
-//   if (amountObj.amount <= k) {
-//     //amount k ya eşit veya küçükse
-//     amountObj.amount = 0;
-//     lastOrder.push(amountObj.no);
-//     tempObj.splice(index,1)
-//     amountObj=tempObj
-//   }
-
-// });
+console.log(getFinalOrder(k, amounts))
